00394b24d13fdab8043dc6bc7082affa9354e736
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-24 11:29:54 -0400

    Agregando conexion y agregando vistas de publicar

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blogs</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="form__container">
+        <form action="" method=post>
+            <textarea name="text" id="textPublish" cols="30" rows="10"></textarea>
+            <br>
+            <input type="file" name=photo id=photo class=photo>
+            <br>
+            <button class="button__publish">Publicar</button>
+        </form>
+    </div>
+
+
+
+
+
+
+</body>
+</html>
+
+
