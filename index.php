e108b33be55cbce426980def389f485382c81764
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-24 14:30:34 -0400

    agregando y estilizando pantalla de publicacion de estados y validando con js los campos, y agregando carpeta de image

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<?php
-
-
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -12,24 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blogs</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="public/css/index.css">
+    <link rel="stylesheet" href="public/css/validation.css">
 </head>
 <body>
-    <div class="form__container">
-        <form action="" method=post>
-            <textarea name="text" id="textPublish" cols="30" rows="10"></textarea>
-            <br>
-            <input type="file" name=photo id=photo class=photo>
-            <br>
-            <button class="button__publish">Publicar</button>
+    <div class="form__container" id="form__container"> 
+        <h1 class=titlecontainer>Publicar un articulo</h1>
+        <form action="" method=post class=form__publish id="form__publish" enctype="multipart/form-data">
+            <input type="text"  placeholder="Titulo de mi articulo"
+            name=title class=title__article id=titleArticle>
+            <textarea name="text" class="textPublish" placeholder="Descripcion de mi articulo"
+             id="textPublish" cols="30" rows="10"></textarea>
+                
+             <p class=containerPhoto>
+                <input type="file" name=photo id=photo class=photo >
+            </p>
+           
+            
+            <button class="button__publish" id=button__publish>Publicar articulo</button>
         </form>
+        
     </div>
 
+<?php
+
+//mostrado de elementos
 
 
 
 
 
+
+
+
+
+
+
+?>
+<script src="public/js/validation.js"></script>
 </body>
 </html>
 
--- php/model/model.php
+++ php/model/model.php
@@ -2,7 +2,7 @@
 	$conection = null;
 	function conected(){
 		global $conection;
-		 $conection = new mysqli("localhost", "jere","0847","jeremias");
+		 $conection = new mysqli("localhost", "jere","0847","Blogs");
 		 if(mysqli_connect_errno($conection)){
 		 	echo "error";
 		 }
