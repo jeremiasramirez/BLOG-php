c8c6bda7c3a81d885ef1bef3a04380d9187eb8f9
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-28 22:08:41 -0400

    Agregando agregador de blogs

--- index.php
+++ index.php
@@ -47,6 +47,23 @@ if(isset($_GET["go"]) && $_GET["go"]=="go"){
 
         if( $conector == true){
 
+            print("<div class=form__container id=form__container> 
+                <h1 class=titlecontainer>Publicar un articulo</h1>
+                <form action='php/validating.php' method=post class=form__publish id=form__publish enctype='multipart/form-data'>
+                   
+                <input type='text'  placeholder='Titulo de mi articulo' name=title class=title__article id=titleArticle>
+
+                        <textarea name='text' class='textPublish' placeholder='Descripcion de mi articulo' id='textPublish' cols='30' rows='10'></textarea>
+                            
+                        <p class=containerPhoto>
+                            <input type='file' name=photo id=photo class=photo >
+                        </p>
+
+                        <button class='button__publish' id=button__publish>Publicar articulo</button>
+
+                </form>
+                
+            </div>");
             }
 
 
