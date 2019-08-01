5dfb849961f61c6e75666039da43c760b44eddeb
Jeremias ramirez <jereramirez499@gmail.com>
2019-07-31 20:52:33 -0400

    Agregado de msj de error en caso de conexion erronea en la db

--- php/model/model.php
+++ php/model/model.php
@@ -2,14 +2,39 @@
 	$conection = null;
 	function conected(){
 		global $conection;
-		 $conection = new mysqli("localhost", "jere","0847","Blogs");
-		 if(mysqli_connect_errno($conection)){
-		 	echo "error";
-		 }
-		 
+
+		try{
+			$conection =mysqli_connect("localhost", "jere", "0847", "Blogs");
+			if(mysqli_connect_errno($conection)){
+				throw new Exception("101");
+			}
+
+		}catch(Exception $e){
+			echo "<p class=error_101>Error " . $e->getMessage() ."</p>" . PHP_EOL;
+		}
+
+
 	}
-	
 	conected();
 
 
 ?>
+<style type="text/css">
+	.error_101{
+
+		color: white;
+		background-color: rgba(100%, 0%, 0%, 95%) ;
+		position:relative;
+		width:100vw;
+		top:0;
+		text-align: center;
+		font-family: arial;
+		font-size: 28px;
+		z-index: 100000;
+		padding-top: 2em;
+		padding-bottom: 2em
+	}
+
+
+
+</style>
